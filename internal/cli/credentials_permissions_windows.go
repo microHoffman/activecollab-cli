fa43a418b942d0bc1c57726432a6b371e6ae89b7
microHoffman <skozak@protonmail.com>
2026-07-21 15:57:39 +0200

    fix: verify mapped Windows file permissions

--- internal/cli/credentials_permissions_windows.go
+++ internal/cli/credentials_permissions_windows.go
@@ -11,6 +11,8 @@ import (
 	"golang.org/x/sys/windows"
 )
 
+const credentialFullControl windows.ACCESS_MASK = windows.STANDARD_RIGHTS_REQUIRED | windows.SYNCHRONIZE | 0x1ff
+
 func protectCredentialPath(path string, _ bool) error {
 	sids, err := credentialSIDs()
 	if err != nil {
@@ -38,7 +40,7 @@ func protectCredentialPath(path string, _ bool) error {
 
 func fullControlEntry(sid *windows.SID, trusteeType windows.TRUSTEE_TYPE) windows.EXPLICIT_ACCESS {
 	return windows.EXPLICIT_ACCESS{
-		AccessPermissions: windows.GENERIC_ALL,
+		AccessPermissions: credentialFullControl,
 		AccessMode:        windows.GRANT_ACCESS,
 		Trustee: windows.TRUSTEE{
 			TrusteeForm:  windows.TRUSTEE_IS_SID,
@@ -81,7 +83,7 @@ func verifyCredentialProtection(path string, _ os.FileInfo) error {
 		if err := windows.GetAce(dacl, index, &ace); err != nil {
 			return err
 		}
-		if ace.Header.AceType != windows.ACCESS_ALLOWED_ACE_TYPE || ace.Mask != windows.GENERIC_ALL {
+		if ace.Header.AceType != windows.ACCESS_ALLOWED_ACE_TYPE || ace.Mask != credentialFullControl {
 			return errors.New("credentials DACL contains an unexpected access rule")
 		}
 		sid := (*windows.SID)(unsafe.Pointer(&ace.SidStart))
